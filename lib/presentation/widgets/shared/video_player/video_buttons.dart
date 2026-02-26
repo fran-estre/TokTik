224d59a1d848f2e1f2f370e049086598899b2f5d
Francisco Estrella <fran.estre@live.com>
2026-02-25 23:15:50 -0500

    Add full-screen video player and update dependencies
    - Implement FullScreenPlayer widget for video playback.
    - Add internet permission in AndroidManifest.xml.
    - Remove delay in loading next page of videos in DiscoverProvider.
    - Update video buttons layout with spacing and animations.
    - Register video player plugin in GeneratedPluginRegistrant.swift.
    - Update pubspec.yaml and pubspec.lock with new dependencies.

--- lib/presentation/widgets/shared/video_player/video_buttons.dart
+++ lib/presentation/widgets/shared/video_player/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/entities/video_post.dart';
@@ -15,10 +16,16 @@ class VideoButtons extends StatelessWidget {
           iconColor: Colors.red,
           iconData: Icons.favorite,
         ),
+        const SizedBox(height: 20),
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
         ),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: _CustomIconButton(value: 0, iconData: Icons.play_circle_outlined)),
       ],
     );
   }
@@ -33,8 +40,7 @@ class _CustomIconButton extends StatelessWidget {
     required this.value,
     required this.iconData,
     iconColor,
-  }): color =iconColor ?? Colors.white;
-
+  }) : color = iconColor ?? Colors.white;
 
   @override
   Widget build(BuildContext context) {
@@ -42,9 +48,9 @@ class _CustomIconButton extends StatelessWidget {
       children: [
         IconButton(
           onPressed: () {},
-          icon: Icon(iconData, color: color,size: 30,),
+          icon: Icon(iconData, color: color, size: 30),
         ),
-        Text(HumanFormats.humanReadbleNumber(value.toDouble())),
+        if (value > 0) Text(HumanFormats.humanReadbleNumber(value.toDouble())),
       ],
     );
   }
--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_player/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/full_screen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -17,8 +18,10 @@ class VideoScrollableView extends StatelessWidget {
         final VideoPost videoPost = videos[index];
         return Stack(
           children: [
+            SizedBox.expand(
+              child:FullScreenPlayer(caption: videoPost.caption,videoUrl: videoPost.videoUrl,)
+            ),
             //videoplayer + gradient
-
             //Botones
             Positioned(
               bottom: 40,
